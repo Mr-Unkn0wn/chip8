c29eedad992bfc1396cb6b24b51789ec0ca98bbd
Jan Frase <jan.frase@ovgu.de>
2023-05-07 20:22:46 +0200

    fixed blurred texture scaling

--- src/interface.rs
+++ src/interface.rs
@@ -3,30 +3,12 @@ use macroquad::prelude::*;
 use crate::chip8::{Chip8, DISPLAY_HEIGHT, DISPLAY_WIDTH};
 
 pub struct Interface {
-    image: Image,
-    texture: Texture2D,
-    texture_params: DrawTextureParams,
+    pixel_scaling: u16,
 }
 
 impl Interface {
     pub fn new() -> Interface {
-        let image = Image::gen_image_color(DISPLAY_WIDTH as u16, DISPLAY_HEIGHT as u16, RED);
-        let texture = Texture2D::from_image(&image);
-        texture.set_filter(FilterMode::Linear);
-        let texture_params = DrawTextureParams {
-            dest_size: Some(Vec2::new(screen_width(), screen_height())),
-            source: None,
-            rotation: 0.0,
-            flip_x: false,
-            flip_y: false,
-            pivot: None,
-        };
-
-        Interface {
-            image,
-            texture,
-            texture_params,
-        }
+        Interface { pixel_scaling: 10 }
     }
 
     pub fn draw_gui(&mut self, chip8: &Chip8) {
@@ -47,11 +29,14 @@ impl Interface {
                     false => BLACK,
                 };
 
-                self.image.set_pixel(x as u32, y as u32, color)
+                draw_rectangle(
+                    (x as u16 * self.pixel_scaling) as f32,
+                    (y as u16 * self.pixel_scaling) as f32,
+                    self.pixel_scaling as f32,
+                    self.pixel_scaling as f32,
+                    color,
+                )
             }
         }
-
-        self.texture.update(&self.image);
-        draw_texture_ex(self.texture, 0.0, 0.0, WHITE, self.texture_params.clone())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use chip8::Chip8;
+use chip8::{Chip8, DISPLAY_HEIGHT, DISPLAY_WIDTH};
 use interface::Interface;
 use macroquad::prelude::*;
 
 mod chip8;
 mod interface;
 
-#[macroquad::main("Chip8")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Chip8".to_string(),
+        fullscreen: false,
+        window_width: (DISPLAY_WIDTH * 10) as i32,
+        window_height: (DISPLAY_HEIGHT * 10) as i32,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf())]
 async fn main() {
     let rom = include_bytes!("E:\\Code\\Rust\\chip8\\roms\\IBM Logo.ch8");
 
